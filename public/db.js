e02c2d6d2f4e921c336c2a73f2c9d6cf24dab9ad
Claholla <claholla@gmail.com>
2021-09-16 17:49:03 -0500

    completed online/offline functionality testing

--- public/db.js
+++ public/db.js
@@ -1,11 +1,11 @@
 // Declare database variable
 let db;
 // Create a new db request for a "budget" database.
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (e) {
     db = e.target.result;
-    db.createObjectStore('newTransaction', { autoIncrement: true });
+    db.createObjectStore("newTransaction", { autoIncrement: true });
 };
 
 request.onerror = function (e) {
@@ -23,10 +23,10 @@ request.onsuccess = function (e) {
 function checkDatabase() {
 
   // Open a transaction on your transaction db
-  const transaction = db.transaction(['newTransaction'], 'readwrite');
+  const transaction = db.transaction(["newTransaction"], "readwrite");
 
   // Access your transaction object
-  const store = transaction.objectStore('newTransaction');
+  const store = transaction.objectStore("newTransaction");
 
   // Get all records from store and set to a variable
   const getAll = store.getAll();
@@ -35,12 +35,12 @@ function checkDatabase() {
   getAll.onsuccess = function () {
     // When the store contains items, they will be bulk added upon reconnecting
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
@@ -48,10 +48,10 @@ function checkDatabase() {
           // If our returned response is not empty
           if (res.length !== 0) {
             // Open another transaction to transaction with the ability to read and write
-            transaction = db.transaction(['newTransaction'], 'readwrite');
+            transaction = db.transaction(["newTransaction"], "readwrite");
 
             // Assign the current store to a variable
-            const currentStore = transaction.objectStore('newTransaction');
+            const currentStore = transaction.objectStore("newTransaction");
 
             // Clear existing entries because our bulk add was successful
             currentStore.clear();
@@ -63,14 +63,14 @@ function checkDatabase() {
 
 const saveRecord = (record) => {
   // Create a transaction on the budget db with readwrite access
-  const transaction = db.transaction(['newTransaction'], 'readwrite');
+  const transaction = db.transaction(["newTransaction"], "readwrite");
 
   // Access your budget object store
-  const store = transaction.objectStore('newTransaction');
+  const store = transaction.objectStore("newTransaction");
 
   // Add record to your store with add method.
   store.add(record);
 };
 
 // Listen for app coming back online
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,68 @@
-{
-    "name": "Progressive-Budget",
-    "short-Name": "ProBudge",
-    "start_url": "/",
-    "background_color": "#ffffff",
-    "theme_color": "#ffffff",
-    "display": "standalone",
-    "icons": [
-        {
-            "src": "./icons/icon-192x192.png",
-            "size": "192x192",
-            "type": "image/png"
-        },
-        {
-            "src": "./icons/icon-512x512.png",
-            "size": "512x512",
-            "type": "image/png"
-        }
-    ]
-}
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/styles.css",
+    "/db.js",
+    "/index.js",
+    "/manifest.json"
+  ];
+  
+  const CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
+  
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches
+        .open(CACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches
+        .keys()
+        .then((keyList) => {
+          return Promise.all(
+              keyList.map(key =>
+                { if (key !== CACHE && key !== RUNTIME_CACHE) {
+                    console.log("Removing old cache data", key);
+                    return caches.delete(key);
+                }})
+            );
+        })
+    );
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then((cache) => {
+          return fetch(event.request)
+            .then(response => {
+                if (response.status === 200) {
+                    cache.put(event.request.url, response.clone())
+                    return response;
+                }
+            })
+            .catch(err => {
+                return cache.patch(event.request)
+            })
+            .catch(err => 
+                console.log(err)
+            )
+        })
+      );
+      return;
+    }
+    event.respondWith(
+        caches.match(event.request)
+        .then (function(response) {
+            return response || fetch(event.request)
+        })
+    )
+  });
